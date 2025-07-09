a525a61420fa3d983b5f0cefc66716cd41e3027a
TanuSingh2003 <tanusinghh03@gmail.com>
2025-07-09 16:05:35 +0530

    WIP: saving local changes before pull

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,46 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
 
-# Create ECR repo to store Docker images
+# Create ECR Repository for Docker image
 resource "aws_ecr_repository" "app_repo" {
-  name = "my-python-app"
+  name = "my-python-app-2"
 }
 
-# Create EKS Cluster
+# Create EKS Cluster using terraform-aws-modules/eks
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "19.21.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.21.0"
 
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.27"  subnet_ids      = ["subnet-082448f700d79379e", "subnet-096e71c1b570c253c"] 
-  vpc_id          = "vpc-0f27864949188e468"  
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  subnet_ids      = var.subnet_ids
+  vpc_id          = var.vpc_id
 
   eks_managed_node_groups = {
     default = {
-      desired_size    = 2
-      max_size        = 3
-      min_size        = 1
-      instance_types  = ["t3.medium"]
+      desired_size   = var.desired_size
+      max_size       = var.max_size
+      min_size       = var.min_size
+      instance_types = var.instance_types
     }
   }
 
-  # Optional for public access
-  enable_irsa = true
-  manage_aws_auth = true
+  enable_irsa        = true
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # must be v5.x or above
+    }
+  }
+
+  required_version = ">= 1.3"
 }
 
